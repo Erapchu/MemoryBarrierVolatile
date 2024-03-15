eff55ac56ed329275466aa001fc19bbc6e860814
Andrey Nasonov <andrewwos88@gmail.com>
2024-03-15 16:23:49 +0300

    Add project files.

---
+++ MemoryBarrierVolatile/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Blocking();
+    }
+
+    private static void Blocking()
+    {
+        bool complete = false;
+        var t = new Thread(() =>
+        {
+            bool toggle = false;
+            while (!complete) toggle = !toggle;
+        });
+        t.Start();
+        Thread.Sleep(1000);
+        complete = true;
+        t.Join();     // blocks
+    }
+}
